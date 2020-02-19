ce205d216337845c7483ada8ef46f72426412df2
zainab.oyedeji <zainab.oyedeji@riby.me>
2020-02-19 16:34:13 +0100

    4h3m

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import React,{Component} from 'react';
-import logo from './logo.svg';
+import {Switch , Route} from 'react-router-dom';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-// import { render } from '@testing-library/react';
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Details from './components/Details';
+import Cart from './components/Cart';
+import Default from './components/Default';
+import Modal from './components/Modal';
+
 
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col-6">
-column number one
-          </div>
-          <div className="col-6"><span><i className="fas fa-home"></i></span></div>
-        </div>
-      </div>
+      <React.Fragment>
+        <Navbar/>
+        <Switch>
+<Route exact path='/' component={ProductList}/>
+<Route exact path='/details' component={Details}/>
+<Route exact path='/cart' component={Cart}/>
+<Route component={Default}/>
+        </Switch>
+        <Modal/>
+        
+      </React.Fragment>
     );
   }
-}
+} 
 
 export default App;
