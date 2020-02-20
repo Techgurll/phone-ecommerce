fd6aebd82606a1f9efeb26fc0516e58b9368f9ae
zainab.oyedeji <zainab.oyedeji@riby.me>
2020-02-20 14:36:21 +0100

    imdone

--- src/data.js
+++ src/data.js
@@ -2,10 +2,10 @@
 export const storeProducts = [
     {
       id: 1,
-      title: "Google Pixel - Black",
-      img: "img/product-1.png",
-      price: 10,
-      company: "GOOGLE",
+      title: "Iphone 11",
+      img: "img/product-2.png",
+      price: 699,
+      company: "APPLE",
       info:
         "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
       inCart: false,
